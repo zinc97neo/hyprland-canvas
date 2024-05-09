12c83e6befec232816f715b1644d8787b8236745
zinc <zinc97neo@proton.me>
2024-05-09 21:16:12 +0800

    feat(global): initial implementation of canvas

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,23 @@
+#![allow(dead_code)]
+#![allow(unused)]
+mod canvas;
+use canvas::Canvas;
 use core::panic;
+use hyprland::{
+    data::{Clients, Monitors},
+    dispatch::{Dispatch, DispatchType, Position, WindowIdentifier},
+    event_listener::EventListener,
+    shared::{Address, HyprData, HyprDataVec},
+    Result,
+};
+use std::sync::Mutex;
 use std::thread;
+#[macro_use]
+extern crate lazy_static;
 
-use hyprland::{event_listener::EventListener, Result};
+lazy_static! {
+    static ref CANVAS: Mutex<Canvas> = Mutex::new(Canvas::new(8, 1440, 900));
+}
 
 fn main() {
     loop {
@@ -24,13 +40,35 @@ fn main() {
 fn register_and_listen() -> Result<()> {
     let mut listener = EventListener::new();
     listener.add_active_window_change_handler(|data| {
-        println!("{:#?}", data);
+        let address = if let Some(data) = data.clone() {
+            Some(data.window_address)
+        } else {
+            None
+        };
+        let canvas = CANVAS.lock();
+        if let Ok(mut canvas) = canvas {
+            canvas.change_active(address);
+        } else if let Err(e) = canvas {
+            panic!("Failed to lock canvas: {e}");
+        }
     });
     listener.add_window_open_handler(|data| {
-        println!("{:?}", data);
+        let address = data.window_address.clone();
+        let canvas = CANVAS.lock();
+        if let Ok(mut canvas) = canvas {
+            canvas.add_window(address);
+        } else if let Err(e) = canvas {
+            panic!("Failed to lock canvas: {e}");
+        }
     });
     listener.add_window_close_handler(|data| {
-        println!("{:?}", data);
+        let address = data.clone();
+        let canvas = CANVAS.lock();
+        if let Ok(mut canvas) = canvas {
+            canvas.remove_window(address);
+        } else if let Err(e) = canvas {
+            panic!("Failed to lock canvas: {e}");
+        }
     });
     listener.start_listener()?;
     Ok(())
