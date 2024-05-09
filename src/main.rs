9fd21f2ed06733b3fe2cbeaeb0132816accbd3f6
zinc <zinc97neo@proton.me>
2024-05-09 11:41:14 +0800

    feat(global): add a thread that listens for hyprland events

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use core::panic;
+use std::thread;
+
+use hyprland::{event_listener::EventListener, Result};
+
 fn main() {
-    println!("Hyprland Canvas!");
+    loop {
+        let handle = thread::spawn(move || {
+            if let Err(e) = register_and_listen() {
+                panic!("Failed to register event listener: {e}");
+            }
+        });
+        match handle.join() {
+            Ok(_) => {
+                println!("unknown condition");
+            }
+            Err(e) => {
+                println!("Failed to join thread: {:?}", e);
+            }
+        }
+    }
+}
+
+fn register_and_listen() -> Result<()> {
+    let mut listener = EventListener::new();
+    listener.add_active_window_change_handler(|data| {
+        println!("{:#?}", data);
+    });
+    listener.add_window_open_handler(|data| {
+        println!("{:?}", data);
+    });
+    listener.add_window_close_handler(|data| {
+        println!("{:?}", data);
+    });
+    listener.start_listener()?;
+    Ok(())
 }
